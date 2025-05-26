7516a9c277f4bdeaca21bf255f65dd9f1d9fb1b6
ngthdan0811 <ngthdan0811@gmail.com>
2025-05-26 17:58:17 +0700

    update : edit package.json file

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -2,21 +2,35 @@
 import config from "./theme.config.json" with { type: "json" };
 
 import myTailwindPreset from '../dist/index.js';
+// import myTailwindPreset from '@viivue/my-preset-tailwind';
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        // background: "var(--background)",
-        // foreground: "var(--foreground)",
-      },
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'blue': '#1fb6ff',
+                'purple': '#7e5bef',
+                'pink': '#ff49db',
+                'orange': '#ff7849',
+                'green': '#13ce66',
+                'yellow': '#ffc82c',
+                'gray-dark': '#273444',
+                'gray': '#8492a6',
+                'gray-light': '#d3dce6',
+            },
+            fontFamily: {
+                sans: ['Graphik', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            },
+            spacing: {
+                '8xl': '96rem',
+                '9xl': '128rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
     },
-  },
-  plugins: [],
 
   presets: [myTailwindPreset(config)],
 };
